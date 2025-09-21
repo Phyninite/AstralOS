3fcd6c694cf022c2ebc38affbbaa8c510a60390d
KERN_SUCCESS <jakajakamd@gmail.com>
2025-09-21 15:59:53 -0500

    Update vfs.h

--- kern/fs/vfs.h
+++ kern/fs/vfs.h
@@ -1,1 +1,13 @@
+#ifndef VFS_H
+#define VFS_H
 
+struct vfs_node;
+
+int vfs_init(void);
+struct vfs_node* vfs_lookup(struct vfs_node *node, const char *name);
+int vfs_mount(struct vfs_node *mount_point, void *fs_root);
+struct vfs_node* vfs_create_node(struct vfs_node *parent, const char *name, int type);
+int vfs_read(struct vfs_node *node, void *buffer, int size);
+int vfs_write(struct vfs_node *node, const void *buffer, int size);
+
+#endif
